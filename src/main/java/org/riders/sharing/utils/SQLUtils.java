71b4ffbc22251c397b1f55aef438046e760afb01
pinkblink <m.pink.blink@gmail.com>
2025-02-20 14:35:08 +0100

    refactor: process of initialization database

--- src/main/java/org/riders/sharing/utils/SQLUtils.java
+++ src/main/java/org/riders/sharing/utils/SQLUtils.java
@@ -2,6 +2,8 @@ package org.riders.sharing.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.riders.sharing.exception.FileNotFoundException;
+import org.riders.sharing.exception.NoSQLConnectionException;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,29 +19,14 @@ public class SQLUtils {
     private static final String SCOOTERS_TABLE_NAME = "scooters";
     private static final String ORDERS_TABLE_NAME = "orders";
 
-    public static void initDatabase() { // i should rework those tasty spaghetti
+    public static void initDatabase() {
         if (SQLValidator.isCreatedDB(DD_RIDERS_URL, USER, PASSWORD)) {
-            logger.info("Database dd_riders_db has already been created;");
-            try (Connection connection = DriverManager.getConnection(DD_RIDERS_URL, USER, PASSWORD)) {
+            createTypesIfNotExists();
+            sendCreateFile(PATH_TO_CREATE_TABLES_FILE
+                    , DD_RIDERS_URL
+                    , USER
+                    , PASSWORD);
 
-                if (SQLValidator.isTableCreated(connection, CUSTOMERS_TABLE_NAME)
-                        && SQLValidator.isTableCreated(connection, SCOOTERS_TABLE_NAME)
-                        && SQLValidator.isTableCreated(connection, ORDERS_TABLE_NAME)) {
-                    logger.info("Tables have already been created;");
-
-                } else {
-
-                    logger.info("Database is created without tables. Attempt to create tables");
-                    sendCreateFile(PATH_TO_CREATE_TABLES_FILE
-                            , DD_RIDERS_URL
-                            , USER
-                            , PASSWORD);
-                    logger.info("Tables were successfully created");
-                }
-            } catch (SQLException e) {
-                logger.error("Error occurred while trying to create tables;", e);
-                throw new RuntimeException(e);
-            }
         } else {
             sendCreateFile(PATH_TO_CREATE_DATABASE_FILE
                     , POSTGRES_URL
@@ -55,10 +42,39 @@ public class SQLUtils {
         }
     }
 
+    private static void createTablesIfNotExists() {
+        try (Connection connection = DriverManager.getConnection(DD_RIDERS_URL, USER, PASSWORD)) {
+            if (SQLValidator.isTableCreated(connection, CUSTOMERS_TABLE_NAME)
+                    && SQLValidator.isTableCreated(connection, SCOOTERS_TABLE_NAME)
+                    && SQLValidator.isTableCreated(connection, ORDERS_TABLE_NAME)) {
+                logger.info("Tables have already been created;");
+
+            } else {
+
+                logger.info("Database is created without tables. Attempt to create tables");
+                sendCreateFile(PATH_TO_CREATE_TABLES_FILE
+                        , DD_RIDERS_URL
+                        , USER
+                        , PASSWORD);
+                logger.info("Tables were successfully created");
+            }
+        } catch (SQLException e) {
+            logger.error("Error occurred while trying to create tables;", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void createTypesIfNotExists() {
+        if (!isTypesExists()) {
+            sendCreateFile(PATH_TO_CREATE_TYPES_FILE, DD_RIDERS_URL, USER, PASSWORD);
+        }
+    }
+
     private static void sendCreateFile(String path, String URL, String user, String password) {
         try (Connection connection = DriverManager.getConnection(URL
                 , user
                 , password)) {
+
             logger.info("Created connection with " + URL);
 
             Statement statement = connection.createStatement();
@@ -83,7 +99,32 @@ public class SQLUtils {
 
         } catch (IOException e) {
             logger.error("File doesn't exist. Path: " + path, e);
-            throw new RuntimeException(e);
+            throw new FileNotFoundException("File doesn't exist. Path: " + path, e);
+        }
+    }
+
+    private static boolean isTypesExists() {
+        String scooterType = "SELECT 1" +
+                "FROM pg_type" +
+                "WHERE typname = 'scooter_type';";
+        String scooterStatus = "SELECT 1" +
+                "FROM pg_type" +
+                "WHERE typname = 'scooter_status';";
+        String orderStatus = "SELECT 1" +
+                "FROM pg_type" +
+                "WHERE typname = 'scooter_status';";
+
+        try (Connection connection = DriverManager.getConnection(DD_RIDERS_URL, USER, PASSWORD);
+             PreparedStatement statementScooterStatus = connection.prepareStatement(scooterStatus);
+             PreparedStatement statementScooterType = connection.prepareStatement(scooterType);
+             PreparedStatement statementOrderStatus = connection.prepareStatement(orderStatus)) {
+
+            boolean isScooterStatus = statementScooterStatus.executeQuery().next();
+            boolean isScooterType = statementScooterType.executeQuery().next();
+            boolean isOrderStatus = statementOrderStatus.executeQuery().next();
+            return isOrderStatus && isScooterType && isScooterStatus;
+        } catch (SQLException e) {
+            throw new NoSQLConnectionException("Error occurred while trying to check sql types");
         }
     }
 }
--- src/main/java/org/riders/sharing/utils/constants/DataBaseInfo.java
+++ src/main/java/org/riders/sharing/utils/constants/DataBaseInfo.java
@@ -5,6 +5,7 @@ public class DataBaseInfo {
     public static final String PASSWORD = "pinkblink";
     public static final String DD_RIDERS_URL = "jdbc:postgresql://localhost:5432/dd_riders_db";
     public static final String POSTGRES_URL = "jdbc:postgresql://localhost:5432/postgres";
-    public static final String PATH_TO_CREATE_DATABASE_FILE = "sql/create_db_script.sql";
-    public static final String PATH_TO_CREATE_TABLES_FILE = "sql/create_tables_script.sql";
+    public static final String PATH_TO_CREATE_DATABASE_FILE =  "/src/main/resources/sql/create_db_script.sql";
+    public static final String PATH_TO_CREATE_TABLES_FILE =  "/src/main/resources/sql/create_tables_script.sql";
+    public static final String PATH_TO_CREATE_TYPES_FILE = "src/main/resources/sql/create_types_script.sql";
 }
