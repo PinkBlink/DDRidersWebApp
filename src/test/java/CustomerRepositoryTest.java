189767b8cfff2ca38da66818ee2811aac5d328c4
pinkblink <m.pink.blink@gmail.com>
2025-03-21 11:46:40 +0100

    force commit (change laptops)

---
+++ src/test/java/CustomerRepositoryTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.riders.sharing.model.Customer;
+import org.riders.sharing.repository.CustomerRepository;
+import org.riders.sharing.repository.impl.CustomerRepositoryImpl;
+
+public class CustomerRepositoryTest {
+    @Test
+    public void customerRepositorySaveTest(){
+//        CustomerRepository customerRepository = new CustomerRepositoryImpl();
+//        Customer customer = Customer.Builder
+//                .getNewBuilderWithId()
+//                .setName("Test1")
+//                .setSurname("Test1")
+//                .setPassword("Test1")
+//                .setEmail("Test1")
+//                .build();
+//        customerRepository.save(customer);
+    }
+}
