4374b8577acda875a38e1ab77b8e6905d44550c9
PinkBlink <m.pink.blink@gmail.com>
2025-03-31 22:51:12 +0200

    refactor: rename FileNotFoundException.java to SQLFileNotFoundException.java refactor: rename method isCreatedDB() to isDatabaseCreated(), add logging for failure cases feat: throw NoSQLConnectionException in getInputStringFromFile instead of RuntimeException

--- src/main/java/org/riders/sharing/utils/SQLUtils.java
+++ src/main/java/org/riders/sharing/utils/SQLUtils.java
@@ -2,7 +2,7 @@ package org.riders.sharing.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.riders.sharing.exception.FileNotFoundException;
+import org.riders.sharing.exception.SQLFileNotFoundException;
 import org.riders.sharing.exception.NoSQLConnectionException;
 
 import java.io.BufferedReader;
@@ -16,7 +16,7 @@ public class SQLUtils {
     private static final Logger logger = LogManager.getLogger(SQLUtils.class);
 
     public static void initDatabase() {
-        if (isCreatedDB(DD_RIDERS_URL, USER, PASSWORD)) {
+        if (isDatabaseCreated(DD_RIDERS_URL, USER, PASSWORD)) {
             logger.info("dd_riders_db is already exists");
             sendCreateFile(PATH_TO_CREATE_TABLES_FILE
                     , DD_RIDERS_URL
@@ -40,11 +40,9 @@ public class SQLUtils {
         }
     }
 
-    private static void sendCreateFile(String path, String URL, String user, String password) {
-        try (Connection connection = DriverManager.getConnection(URL
-                , user
-                , password)) {
-            logger.info("Created connection with " + URL);
+    private static void sendCreateFile(String path, String url, String user, String password) {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            logger.info("Created connection with {}", url);
 
             InputStream inputStream = getInputStreamFromFile(path);
             Statement statement = connection.createStatement();
@@ -55,17 +53,19 @@ public class SQLUtils {
 
             logger.info("The script is successfully sent;");
         } catch (SQLException e) {
-            logger.error(e);
-            throw new RuntimeException();
+            logger.error("Couldn't create connection to database with URL: {}", url, e);
+            throw new NoSQLConnectionException("Couldn't create connection to database with URL:" + url, e);
         }
     }
 
     private static InputStream getInputStreamFromFile(String path) {
         InputStream inputStream = SQLUtils.class.getClassLoader().getResourceAsStream(path);
+
         if (inputStream == null) {
-            logger.error("File doesn't exist. Path: " + path);
-            throw new FileNotFoundException("File doesn't exist. Path: " + path);
+            logger.error("File doesn't exist. Path: {}", path);
+            throw new SQLFileNotFoundException("File doesn't exist. Path: " + path);
         }
+
         return inputStream;
     }
 
@@ -79,12 +79,11 @@ public class SQLUtils {
         return stringBuilder.toString().trim();
     }
 
-    private static boolean isCreatedDB(String URL, String user, String password) {
-        try (Connection connection = DriverManager.getConnection(URL
-                , user
-                , password)) {
+    private static boolean isDatabaseCreated(String url, String user, String password) {
+        try (Connection maybeConnection = DriverManager.getConnection(url, user, password)) {
             return true;
         } catch (SQLException e) {
+            logger.warn("Database is not created. URL: {}", url);
             return false;
         }
     }
