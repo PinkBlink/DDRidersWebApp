99f7455c1394693abe3531a56f773449ec49abf1
PinkBlink <m.pink.blink@gmail.com>
2025-04-02 18:23:14 +0200

    Add tests for all ScooterRepository methods and create TestUtils for cleaning test entities from the database. All tests passed.

---
+++ src/test/java/ScooterRepositoryTests.java
@@ -0,0 +1,186 @@
+import org.junit.jupiter.api.*;
+import org.riders.sharing.exception.DuplicateIdException;
+import org.riders.sharing.model.Scooter;
+import org.riders.sharing.model.enums.ScooterStatus;
+import org.riders.sharing.model.enums.ScooterType;
+import org.riders.sharing.repository.ScooterRepository;
+import org.riders.sharing.repository.impl.ScooterRepositoryImpl;
+
+
+import java.sql.SQLException;
+import java.util.*;
+
+public class ScooterRepositoryTests {
+    private final ScooterRepository scooterRepository = new ScooterRepositoryImpl();
+
+    private static final Scooter SCOOTER_1 = Scooter.Builder.getNewBuilderWithId()
+            .setScooterType(ScooterType.FOLDING)
+            .setStatus(ScooterStatus.AVAILABLE)
+            .setBatteryLevel(100)
+            .build();
+
+    private static final Scooter SCOOTER_2 = Scooter.Builder.getNewBuilderWithId()
+            .setScooterType(ScooterType.LONG_RANGE)
+            .setStatus(ScooterStatus.AVAILABLE)
+            .setBatteryLevel(100)
+            .build();
+
+    @BeforeAll
+    public static void beforeAll() throws SQLException {
+        TestsUtils.deleteEntitiesFromDatabase(TestsUtils.SCOOTERS_TABLE, SCOOTER_1, SCOOTER_2);
+    }
+
+
+    @AfterAll
+    public static void afterAll() throws SQLException {
+        TestsUtils.deleteEntitiesFromDatabase(TestsUtils.SCOOTERS_TABLE, SCOOTER_1, SCOOTER_2);
+    }
+
+    @AfterEach
+    public void afterEach() throws SQLException {
+        TestsUtils.deleteEntitiesFromDatabase(TestsUtils.SCOOTERS_TABLE, SCOOTER_1, SCOOTER_2);
+    }
+
+    @Test
+    public void saveShouldSetCreateAndUpdateTime() {
+        Scooter savedScooter = scooterRepository.save(SCOOTER_1);
+
+        Assertions.assertTrue(savedScooter.getCreateTime().equals(savedScooter.getUpdateTime())
+                && SCOOTER_1.equals(savedScooter));
+    }
+
+    @Test
+    public void saveShouldThrowIfEqualsIds() {
+        scooterRepository.save(SCOOTER_1);
+
+        Assertions.assertThrows(DuplicateIdException.class, () -> scooterRepository.save(SCOOTER_1));
+    }
+
+    @Test
+    public void findShouldReturnScooter() {
+        scooterRepository.save(SCOOTER_1);
+
+        Scooter scooterFromDatabase = scooterRepository.find(SCOOTER_1.getId()).get();
+
+        Assertions.assertEquals(SCOOTER_1, scooterFromDatabase);
+    }
+
+    @Test
+    public void findShouldReturnEmptyOptional() {
+        Optional<Scooter> maybeScooter = scooterRepository.find(UUID.randomUUID());
+
+        Assertions.assertThrows(NoSuchElementException.class, maybeScooter::get);
+    }
+
+    @Test
+    public void findScootersByStatusShouldReturnScooterList() throws SQLException {
+        ScooterStatus status = ScooterStatus.RENTED;
+        List<Scooter> scooters = Arrays.asList(
+                Scooter.Builder.getNewBuilderWithId()
+                        .setScooterType(ScooterType.URBAN)
+                        .setStatus(status)
+                        .build()
+                ,
+                Scooter.Builder.getNewBuilderWithId()
+                        .setScooterType(ScooterType.URBAN)
+                        .setStatus(status)
+                        .build()
+                ,
+                Scooter.Builder.getNewBuilderWithId()
+                        .setScooterType(ScooterType.URBAN)
+                        .setStatus(status)
+                        .build()
+        );
+        scooters.forEach(scooterRepository::save);
+
+        List<Scooter> scootersFromDatabase = scooterRepository.findScootersByStatus(status);
+
+        Assertions.assertEquals(scooters, scootersFromDatabase);
+
+        for (Scooter scooter : scootersFromDatabase) {
+            TestsUtils.deleteEntityFromDatabase(TestsUtils.SCOOTERS_TABLE, scooter);
+        }
+    }
+
+    @Test
+    public void findScootersByStatusEmailShouldReturnEmptyList() {
+        scooterRepository.save(SCOOTER_1);
+
+        List<Scooter> scooters = scooterRepository.findScootersByStatus(ScooterStatus.RENTED);
+
+        Assertions.assertEquals(0, scooters.size());
+    }
+
+    @Test
+    public void findAllShouldReturnAll() {
+        scooterRepository.save(SCOOTER_1);
+        scooterRepository.save(SCOOTER_2);
+
+        List<Scooter> scootersFromDatabase = scooterRepository.findAll();
+
+        Assertions.assertEquals(2, scootersFromDatabase.size());
+    }
+
+    @Test
+    public void updateShouldReturnUpdatedScooter() {
+        ScooterType newScooterType = ScooterType.URBAN;
+        scooterRepository.save(SCOOTER_1);
+
+        Scooter updatedScooter = SCOOTER_1.toBuilder()
+                .setScooterType(newScooterType)
+                .build();
+        scooterRepository.update(updatedScooter);
+        Scooter scooterFromDataBase = scooterRepository.find(updatedScooter.getId()).get();
+
+        Assertions.assertEquals(updatedScooter, scooterFromDataBase);
+    }
+
+    @Test
+    public void updateShouldSetUpdateTime() throws InterruptedException {
+        Scooter savedScooter = scooterRepository.save(SCOOTER_1);
+
+        Thread.sleep(10);
+
+        Scooter updatedScooter = scooterRepository.update(savedScooter);
+
+        String errorMessage = "Saved: %s  Updated: %s "
+                .formatted(savedScooter.getUpdateTime(), updatedScooter.getUpdateTime());
+
+        Assertions.assertTrue(savedScooter.getUpdateTime().isBefore(updatedScooter.getUpdateTime()), errorMessage);
+    }
+
+
+    @Test
+    public void isExistShouldReturnFalse() {
+        Scooter scooter = Scooter.Builder.getNewBuilderWithId().build();
+
+        boolean result = scooterRepository.isExist(scooter);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void isExistShouldReturnTrue() {
+        scooterRepository.save(SCOOTER_1);
+
+        boolean result = scooterRepository.isExist(SCOOTER_1);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void deleteShouldReturnTrue() {
+        scooterRepository.save(SCOOTER_1);
+
+        boolean result = scooterRepository.delete(SCOOTER_1.getId());
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void deleteShouldReturnFalse() {
+        boolean result = scooterRepository.delete(SCOOTER_2.getId());
+
+        Assertions.assertFalse(result);
+    }
+}
---
+++ src/test/java/TestsUtils.java
@@ -0,0 +1,42 @@
+import org.riders.sharing.connection.ConnectionPool;
+import org.riders.sharing.exception.BadDatabaseUpdateException;
+import org.riders.sharing.model.BaseEntity;
+import org.riders.sharing.model.Customer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class TestsUtils {
+    public static final String SCOOTERS_TABLE = "scooters";
+
+    public static void deleteEntitiesFromDatabase(String tableName, BaseEntity... entities) throws SQLException {
+        for (BaseEntity entity : entities) {
+            deleteEntityFromDatabase(tableName, entity);
+        }
+    }
+
+    public static void deleteEntityFromDatabase(String tableName, BaseEntity entity) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ConnectionPool connectionPool = ConnectionPool.INSTANCE;
+
+        try {
+            connection = connectionPool.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "DELETE FROM %s WHERE id = ?".formatted(tableName));
+            preparedStatement.setObject(1, entity.getId(), Types.OTHER);
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new BadDatabaseUpdateException("Something goes wrong.", e);
+        } finally {
+            connectionPool.releaseConnection(connection);
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+    }
+}
