33000225d4ca003d0a34997e2ae49cbe020c1cc6
PinkBlink <m.pink.blink@gmail.com>
2025-04-03 16:30:44 +0200

    Add setting custom url to ConnectionPool

--- src/main/java/org/riders/sharing/connection/ConnectionPool.java
+++ src/main/java/org/riders/sharing/connection/ConnectionPool.java
@@ -9,17 +9,14 @@ import org.riders.sharing.utils.constants.DataBaseInfo;
 import java.sql.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.concurrent.locks.ReentrantLock;
 
 public enum ConnectionPool {
     INSTANCE;
-    private static final ReentrantLock lock = new ReentrantLock();
     private static final int DEFAULT_CAPACITY = 16;
 
     private final Logger logger;
     private Queue<Connection> availableConnections;
     private Queue<Connection> busyConnections;
-
 
     ConnectionPool() {
         logger = LogManager.getLogger(ConnectionPool.class);
@@ -32,7 +29,7 @@ public enum ConnectionPool {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new NoSQLConnectionException(e.getMessage());
         }
     }
 
@@ -44,40 +41,42 @@ public enum ConnectionPool {
                         DriverManager.deregisterDriver(driver);
                     } catch (SQLException e) {
                         logger.error("Can't deregister driver\n" + e);
-                        throw new RuntimeException(e);
+                        throw new NoSQLConnectionException(e.getMessage());
                     }
                 });
     }
 
     private void initConnections() {
+        initConnections(DataBaseInfo.DD_RIDERS_URL, DataBaseInfo.USER, DataBaseInfo.PASSWORD);
+    }
+
+    private void initConnections(String url, String user, String password) {
         availableConnections = new ArrayDeque<>(DEFAULT_CAPACITY);
         busyConnections = new ArrayDeque<>();
+
         try {
             for (int i = 0; i < DEFAULT_CAPACITY; i++) {
-                Connection connection = createConnection();
+                Connection connection = createConnection(url, user, password);
                 availableConnections.add(connection);
             }
         } catch (NoSQLConnectionException e) {
             logger.error(e.getMessage());
         }
+
         if (availableConnections.isEmpty()) {
             logger.error("Available connections amount is zero;");
             throw new RuntimeException("No connections to database");
         } else {
             logger.info("Connections successfully created: " + availableConnections.size());
         }
-
     }
 
-    private Connection createConnection() throws NoSQLConnectionException {
+    private Connection createConnection(String url, String user, String password) throws NoSQLConnectionException {
         Connection connection;
+
         try {
-            connection = DriverManager.getConnection(DataBaseInfo.DD_RIDERS_URL
-                    , DataBaseInfo.USER
-                    , DataBaseInfo.PASSWORD);
-
+            connection = DriverManager.getConnection(url, user, password);
             logger.info("Create new connection: " + connection.toString());
-
         } catch (SQLException e) {
             logger.error("Can't create connection: " + e);
             throw new NoSQLConnectionException(e.getMessage());
@@ -85,27 +84,21 @@ public enum ConnectionPool {
         return connection;
     }
 
-    public Connection getConnection() {
-        lock.lock();
-        try {
-            Connection connection;
-            connection = availableConnections.peek();
-            busyConnections.offer(connection);
+    public void setDatabaseURL(String url, String user, String password) {
+        initConnections(url, user, password);
+    }
 
-            return connection;
-        } finally {
-            lock.unlock();
-        }
+    public synchronized Connection getConnection() {
+        Connection connection;
+        connection = availableConnections.peek();
+        busyConnections.offer(connection);
+
+        return connection;
     }
 
     public void releaseConnection(Connection connection) {
-        lock.lock();
-        try {
-            busyConnections.remove(connection);
-            availableConnections.offer(connection);
-        } finally {
-            lock.unlock();
-        }
+        busyConnections.remove(connection);
+        availableConnections.offer(connection);
     }
 
     public void destroyPool() {
@@ -113,6 +106,7 @@ public enum ConnectionPool {
             for (Connection connection : availableConnections) {
                 connection.close();
             }
+
             for (Connection connection : busyConnections) {
                 connection.close();
             }
