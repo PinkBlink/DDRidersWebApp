cb6fdc9327186a1351c0d1b5cb578bb742118a49
PinkBlink <m.pink.blink@gmail.com>
2025-04-03 16:52:26 +0200

    Remove redundant methods from base repository

--- src/main/java/org/riders/sharing/repository/BaseRepository.java
+++ src/main/java/org/riders/sharing/repository/BaseRepository.java
@@ -1,10 +1,7 @@
 package org.riders.sharing.repository;
 
-import org.apache.logging.log4j.LogManager;
 import org.riders.sharing.model.BaseEntity;
 
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,21 +11,9 @@ public interface BaseRepository<T extends BaseEntity> {
 
     T update(T entity);
 
-    Optional<T> find(UUID id);
+    Optional<T> findById(UUID id);
 
     List<T> findAll();
 
-    boolean isExist(T entity);
-
     T delete(UUID id);
-
-    default void closeStatement(Statement statement) {
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                LogManager.getLogger(this).error("Can't close statement", e);
-            }
-        }
-    }
 }
