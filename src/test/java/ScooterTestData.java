232cece9b13242b9434cc89686d93483382e44cf
PinkBlink <m.pink.blink@gmail.com>
2025-04-08 21:05:31 +0200

    Rework tests according to the comments

--- src/test/java/ScooterTestData.java
+++ src/test/java/ScooterTestData.java
@@ -5,7 +5,7 @@ import org.riders.sharing.model.enums.ScooterType;
 import static org.riders.sharing.model.Scooter.Builder.*;
 
 public interface ScooterTestData {
-    static Builder aScooter() {
+    default Builder aScooter() {
         return scooter()
             .type(ScooterType.URBAN)
             .status(ScooterStatus.AVAILABLE)
