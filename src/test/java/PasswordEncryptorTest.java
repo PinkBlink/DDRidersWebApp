9350235a382dd687c77896f3fee4717916a95ec3
PinkBlink <m.pink.blink@gmail.com>
2025-04-21 12:26:28 +0200

    Add negative test

--- src/test/java/PasswordEncryptorTest.java
+++ src/test/java/PasswordEncryptorTest.java
@@ -2,14 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.riders.sharing.utils.PasswordEncryptor;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordEncryptorTest {
     @Test
-    public void encryptPasswordReturnsSameIfSameInput(){
+    public void encryptPasswordReturnsSameHash() {
         final var password = "secret pass";
 
         final var encryptedPassword1 = PasswordEncryptor.encryptPassword(password);
         final var encryptedPassword2 = PasswordEncryptor.encryptPassword(password);
 
-        Assertions.assertEquals(encryptedPassword1, encryptedPassword2);
+        assertEquals(encryptedPassword1, encryptedPassword2);
+    }
+
+    @Test
+    public void encryptPasswordReturnsDifferentHash() {
+        final var firstPassword = "first-pass";
+        final var secondPassword = "second-pass";
+
+        final var encryptedFirst = PasswordEncryptor.encryptPassword(firstPassword);
+        final var encryptedSecond = PasswordEncryptor.encryptPassword(secondPassword);
+
+        assertNotEquals(encryptedFirst, encryptedSecond);
     }
 }
