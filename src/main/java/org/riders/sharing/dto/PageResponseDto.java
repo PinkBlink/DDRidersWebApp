4b8a0f621b6009e132a620b2f5c9fd7909117085
PinkBlink <m.pink.blink@gmail.com>
2025-04-30 23:12:08 +0200

    refactor

--- src/main/java/org/riders/sharing/dto/PageResponseDto.java
+++ src/main/java/org/riders/sharing/dto/PageResponseDto.java
@@ -2,6 +2,6 @@ package org.riders.sharing.dto;
 
 import java.util.List;
 
-public record PageResponseDto<T extends EntityDto>(List<T> elements, int page, int pageSize,
-                                                   int totalElements, int totalPages) {
+public record PageResponseDto<T extends Pageable>(List<T> elements, int page, int pageSize,
+                                                  int totalElements, int totalPages) {
 }
